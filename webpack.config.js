b12c3265fbf0e575f4d93fc157145c1df4165083
Ramon V <rvicentediaz@gmail.com>
2019-11-07 12:42:50 +0100

    First commit, configuracio per poder executar el codi i canvis dinamics via live-server previa transpilacio. Nomes dev, potser canvia estructura

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  watch:true
+}
